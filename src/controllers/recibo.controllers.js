98d253729378bf7e4534fcfe20bf029bcf12de39
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-09-27 12:57:17 -0500

    cambios en los campos de las tablas

--- src/controllers/recibo.controllers.js
+++ src/controllers/recibo.controllers.js
@@ -1,6 +1,7 @@
 import { getConection, sql, queries } from "../database";
 
 export const getRecibo = async (req, res) => {
+  
   try {
     const pool = await getConection();
     const resultado = await pool.request().query(queries.getAllRec);
@@ -15,12 +16,12 @@ export const createNewRec = async (req, res) => {
   const {
     ESTADO,
     TITULAR,
-    PESO,
-    FECHA_CREADO,
+    PESO,    
     PRECIO,
     PRECIO_U,
     CREADO,
     DIRECCION,
+    FECHA_CREADO
   } = req.body;
   //console.log(req.body)
   try {
@@ -29,22 +30,22 @@ export const createNewRec = async (req, res) => {
       .request()
       .input("ESTADO", sql.VarChar, ESTADO)
       .input("TITULAR", sql.VarChar, TITULAR)
-      .input("PESO", sql.Numeric, PESO)
-      .input("FECHA_CREADO", sql.VarChar, FECHA_CREADO)
+      .input("PESO", sql.Numeric, PESO)      
       .input("PRECIO", sql.Numeric, PRECIO)
       .input("PRECIO_U", sql.Numeric, PRECIO_U)
-      .input("CREADO", sql.VarChar, CREADO)
+      .input("NOMBRE_USUARIO", sql.VarChar, CREADO)
       .input("DIRECCION", sql.VarChar, DIRECCION)
+      .input("FECHA_CREADO", sql.VarChar, FECHA_CREADO)
       .query(queries.createNewRec);
     res.json({
       ESTADO,
       TITULAR,
-      PESO,
-      FECHA_CREADO,
+      PESO,      
       PRECIO,
       PRECIO_U,
       CREADO,
       DIRECCION,
+      FECHA_CREADO
     });
   } catch (error) {
     res.status(500);
@@ -53,16 +54,18 @@ export const createNewRec = async (req, res) => {
 };
 
 export const getRecById = async (req, res) => {
-  console.log("xdtest");
+  console.log('entra')
   try {
-    const { CONSECUTIVOS } = req.params;
+    const { CONSECUTIVOS } = req.params; 
+    console.log(CONSECUTIVOS) 
     const pool = await getConection();
     const result = await pool
       .request()
       .input("CONSECUTIVO", CONSECUTIVOS)
-      .query(queries.getIdRec);
+      .query(queries.getIdRec);      
     res.send(result.recordsets[0]);
   } catch (error) {
+    console.log(error)
     res.status(500);
     res.send(error.messages);
   }
@@ -99,12 +102,12 @@ export const upDateRec = async (req, res) => {
   const {
     ESTADO,
     TITULAR,
-    PESO,
-    FECHA_CREADO,
+    PESO,    
     PRECIO,
     PRECIO_U,
     CREADO,
     DIRECCION,
+    FECHA_CREADO
   } = req.body;
   const { CONSECUTIVOS } = req.params;
   console.log(CONSECUTIVOS)
@@ -117,13 +120,13 @@ export const upDateRec = async (req, res) => {
   await pool.request()  
   .input("ESTADO", sql.VarChar, ESTADO)
   .input("TITULAR", sql.VarChar, TITULAR)
-  .input("PESO", sql.Numeric, PESO)
-  .input("FECHA_CREADO", sql.VarChar, FECHA_CREADO)
+  .input("PESO", sql.Numeric, PESO)  
   .input("PRECIO", sql.Numeric, PRECIO)
   .input("PRECIO_U", sql.Numeric, PRECIO_U)
   .input("CREADO", sql.VarChar, CREADO)
   .input("DIRECCION", sql.VarChar, DIRECCION)
   .input("CONSECUTIVOS", sql.Int, CONSECUTIVOS)
+  .input("FECHA_CREADO", sql.VarChar, FECHA_CREADO)
   .query(queries.upDateRec)
-  res.json({ESTADO,TITULAR,PESO,FECHA_CREADO,PRECIO,PRECIO_U,CREADO,DIRECCION})
+  res.json({ESTADO,TITULAR,PESO,PRECIO,PRECIO_U,CREADO,DIRECCION,FECHA_CREADO})
 };
